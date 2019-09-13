f28ac0f542406896143efaef959518a5be79bbdd
zerobell-lee <bnb3456@gmail.com>
2019-09-13 18:44:11 +0900

    feat: initial commit

---
+++ src/main/java/h3s1/tubesday/TubesdayApplication.java
@@ -0,0 +1,13 @@
+package h3s1.tubesday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesdayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TubesdayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/h3s1/tubesday/TubesdayApplicationTests.java
@@ -0,0 +1,16 @@
+package h3s1.tubesday;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TubesdayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
