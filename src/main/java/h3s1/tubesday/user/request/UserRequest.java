35c117276a3d6933f5e892d54e05caf6a8eac443
zerobell-lee <bnb3456@gmail.com>
2019-09-15 17:28:47 +0900

    fix: Modify name of UserCreateRequest

--- src/main/java/h3s1/tubesday/user/request/UserRequest.java
+++ src/main/java/h3s1/tubesday/user/request/UserRequest.java
@@ -3,7 +3,8 @@ package h3s1.tubesday.user.request;
 import lombok.Data;
 
 @Data
-public class UserCreateRequest {
+public class UserRequest {
+    private Long userNo; // this is nullable. Only for update
     private String email;
     private String nickname;
     private String password;
